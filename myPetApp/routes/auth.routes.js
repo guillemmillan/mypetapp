cf019846e60274446936e2cd96c8cf3f4c6fcd05
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2020-06-16 08:01:01 +0200

    add password validation regex

--- myPetApp/routes/auth.routes.js
+++ myPetApp/routes/auth.routes.js
@@ -16,6 +16,14 @@ router.post("/signup", async (req, res, next) => {
         "All fields are mandatory. Please provide your username, email and password.",
     });
   }
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  const hasIncorrectPassCriteria = !passwordRegex.test(password);
+  if (hasIncorrectPassCriteria) {
+    return res.status(400).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+  }
   try {
     const saltRounds = 10;
     const salt = await bcryptjs.genSalt(saltRounds);
