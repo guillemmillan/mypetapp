e429e6f3ab5f3ce2951d50605e5733ae3a9a68c9
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-06-18 21:37:30 +0200

    Se muestran los places

--- myPetApp/models/User.model.js
+++ myPetApp/models/User.model.js
@@ -4,32 +4,37 @@ const pwdRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 
 const userSchema = new Schema({
   email: {
-      type: String,
-    required: true, 
+    type: String,
+    required: true,
     unique: true
   },
-  username:{
-      type: String,
-        required: true,
-        unique: true
-    },
-  password:{
-      type: String,
-    match:[
-        pwdRegex,
-        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."],
-        required: true,
-    },
-  image:{
-      type: String,
-    default:'../images/user-default.png'
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    match: [
+      pwdRegex,
+      "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
+    ],
+    required: true,
+  },
+  image: {
+    type: String,
+    default: '../images/user-default.png'
     //default:'https://raw.githubusercontent.com/guillemmillan/mypetapp/master/myPetApp/public/images/user-default.png'
-    },
-    location: String,
+  },
+  location: String,
   /*pets:{
         type: Array,
         ref:'Pet'
     }*/
+  parques: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Place"
+  }]
 });
 
 const User = mongoose.model("User", userSchema);
--- myPetApp/routes/index.routes.js
+++ myPetApp/routes/index.routes.js
@@ -1,21 +1,39 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const bcrypt = require('bcryptjs')
+const Place = require('../models/Place.model.js')
+//const authRouter = require('./auth.routes');
+//Registro de partials
 
+//hbs.registerPartials(__dirname + '/partials/places')
+
+//modelo de parques
+
+const User = require("../models/Place.model");
+const mongoose = require("mongoose");
+
+router.get("/signup", async (req, res) => res.render("auth/signup"));
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  //console.log(Place.find())
+  Place.find()
+    .then(allPlaces => {
+      res.render('index', {
+        places: allPlaces
+      });
+    })
+
 });
 
-router.get('/places', (req, res)=> {
-  res.render('places/allplaces');
-});
+// router.get('/places', (req, res) => {
+//   res.render('places/allplaces');
+// });
 
-router.get('/aboutus', (req, res)=> {
+router.get('/aboutus', (req, res) => {
   res.render('about');
 });
 
-router.get('/contact', (req, res)=> {
+router.get('/contact', (req, res) => {
   res.render('contact');
 });
 
