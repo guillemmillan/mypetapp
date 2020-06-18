cae4247773f2153de6cb08a88ececff6711b4099
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-06-18 23:38:49 +0200

    Fixed button

--- myPetApp/routes/auth.routes.js
+++ myPetApp/routes/auth.routes.js
@@ -57,7 +57,9 @@ router.post('/signup', (req, res, next) => {
 });
 module.exports = router;*/
 
-const { Router } = require("express");
+const {
+  Router
+} = require("express");
 const router = new Router();
 const bcryptjs = require("bcryptjs");
 
@@ -66,18 +68,21 @@ const mongoose = require("mongoose");
 router.get("/signup", async (req, res) => res.render("auth/signup"));
 
 router.get("/start", async (req, res) => {
-const users = await User.find()
-console.log(users)
- res.render("star")
+  const users = await User.find()
+  console.log(users)
+  res.render("star")
 });
 
 router.post("/signup", async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const {
+    username,
+    email,
+    password
+  } = req.body;
   const hasEmptyRequiredField = !username || !email || !password;
   if (hasEmptyRequiredField) {
-    return res.render("auth/signup",  {
-     errorMessage:
-        "All fields are mandatory. Please provide your username, email and password.",
+    return res.render("auth/signup", {
+      errorMessage: "All fields are mandatory. Please provide your username, email and password.",
     });
   }
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
@@ -85,8 +90,7 @@ router.post("/signup", async (req, res, next) => {
   if (hasIncorrectPassCriteria) {
     //console.log("HA PASADO LA CLAVE")
     return res.status(400).render("auth/signup", {
-      errorMessage:
-        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      errorMessage: "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
     });
   }
   try {
@@ -110,12 +114,13 @@ router.post("/signup", async (req, res, next) => {
     if (hasValidationError) {
       return res
         .status(400)
-        .render("auth/signup", { errorMessage: error.message });
+        .render("auth/signup", {
+          errorMessage: error.message
+        });
     }
     if (hasDuplicateUniqueField) {
       return res.status(400).render("auth/signup", {
-        errorMessage:
-          "Username and email need to be unique. Either username or email is already used.",
+        errorMessage: "Username and email need to be unique. Either username or email is already used.",
       });
     }
     next(error);
@@ -124,11 +129,11 @@ router.post("/signup", async (req, res, next) => {
 
 
 //ruta de login
-router.get('/logout', function(req, res, next) {
+router.get('/logout', function (req, res, next) {
   if (req.session) {
     // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
+    req.session.destroy(function (err) {
+      if (err) {
         return next(err);
       } else {
         return res.redirect('/');
@@ -141,34 +146,46 @@ router.get('/login', (req, res) => res.render('auth/login'));
 
 // .post() login route ==> to process form data
 router.post('/login', (req, res, next) => {
-  const { email, password } = req.body;
+  const {
+    email,
+    password
+  } = req.body;
   if (email === '' || password === '') {
     res.render('auth/login', {
       errorMessage: 'Please enter both, email and password to login.'
     });
     return;
   }
-  User.findOne({ email })
+  User.findOne({
+      email
+    })
     .then(user => {
       if (!user) {
-        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        res.render('auth/login', {
+          errorMessage: 'Email is not registered. Try with other email.'
+        });
         return;
       } else if (bcryptjs.compareSync(password, user.password)) {
         /*res.render('user-profile', { user });*/
         req.session.currentUser = user;
         res.redirect('userProfile')
       } else {
-        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        res.render('auth/login', {
+          errorMessage: 'Incorrect password.'
+        });
       }
     })
     .catch(error => next(error));
 });
 
 
-router.get("/", (req,res) => res.render("index"))
+router.get("/", (req, res) => res.render("index"))
 
 
 router.get('/userProfile', (req, res) => {
-  res.render('users/user-profile', { userInSession: req.session.currentUser });
+  res.render('users/user-profile', {
+    userInSession: req.session.currentUser
+  });
 });
 module.exports = router;
+/* */
