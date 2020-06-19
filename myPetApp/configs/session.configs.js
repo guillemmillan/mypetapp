b7f8601ed821bd0aaf0f1dd4a441d4cf0a5f9899
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2020-06-19 09:23:38 +0200

    add env variables

--- myPetApp/configs/session.configs.js
+++ myPetApp/configs/session.configs.js
@@ -1,12 +1,12 @@
-const session = require('express-session');
+const session = require("express-session");
 // vamos a utilizar este middleware en app.js,por lo tanto lo tenemos que exportar
-module.exports = app => {
+module.exports = (app) => {
   app.use(
     session({
-      secret: "kdfnsakfn3",
+      secret: process.env.SESS_SECRET,
       resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 6000000 } // 60 * 1000 ms === 1 min
+      cookie: { maxAge: 6000000 }, // 60 * 1000 ms === 1 min
     })
   );
 };
